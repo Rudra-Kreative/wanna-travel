68784488d3ab32c9c80bd973d643e3a99cd3a7a5
Rudra-Kreative <rudraprasadrath@kreativemachinez.net>
2022-07-25 15:52:50 +0530

    Is wanna for me section done in this branch

---
+++ public/admin/dist/js/pages/is_wanna_for_me.js
@@ -0,0 +1,284 @@
+$(document).on('change','#section_select',function(){
+    $('#section_image').val(null);
+    if ($(this).val() !== '' && $(this).val() !== 'undefined')
+    {
+        var section_data = $('#is_wanna_data').val().trim();
+        var this_sec = $(this).val().trim();
+        if(section_data !== '' && section_data !=='undefined')
+        {
+            section_data = JSON.parse(section_data);
+            var isFound = false;
+            $.each(section_data, function (index, value) { 
+
+                 if(value.section == this_sec)
+                 {
+                    $('#section_text').val(value.section_text);
+                    var imgHtml = "<div class='row'><div class='col-md-4'>";
+                    imgHtml+= "<img class='rounded previewImg' style='width: 100%; height:200px' src='/"+value.section_image[0].filePath+"'></div></div>";
+
+                    $('#section_image_preview').html(imgHtml);
+                    $('#section_image_preview_sec').fadeIn(600);
+                    $('#sec_button').html('Update');
+                    $('#is_wanna_for_me').attr('data-action', '/administrator/pages/' + $('#is_wanna_for_me').attr('data-slug') + '/update');
+                    $('#sec_button').attr('disabled',false);
+                    $('#sec_button').attr('data-method', 'update');
+                    $('.section_detailed_div').fadeIn(600);
+                    isFound = true;
+                    return false;
+                 }
+            });
+
+            if(!isFound)
+            {
+                $('#section_text').val('');
+                $('#section_image_preview').html('');
+                $('#sec_button').html('Create');
+                $('#sec_button').attr('data-method', 'create');
+                $('#is_wanna_for_me').attr('data-action', '/administrator/pages/store');
+                $('#sec_button').attr('disabled',false);
+                $('.section_detailed_div').fadeIn(600);
+            }
+        }
+        else
+        {
+            $('#is_wanna_for_me').attr('data-action', '');
+            $('#sec_button').attr('data-method', '');
+            $('#sec_button').html('No action permitted');
+            $('#section_text').val('');
+            $('#section_image_preview').html('');
+            $('#sec_button').attr('disabled',true);
+            $('.section_detailed_div').fadeOut(600);
+        }
+        
+    }
+    else
+    {
+        $('#is_wanna_for_me').attr('data-action', '');
+        $('#sec_button').attr('data-method', '');
+        $('#sec_button').html('No action permitted');
+        $('#section_text').val('');
+        $('#section_image_preview').html('');
+        $('#sec_button').attr('disabled',true);
+        $('.section_detailed_div').fadeOut(600);
+    }
+
+});
+
+
+$(document).on('click','#sec_button',function(){
+
+    if ($(this).attr('data-method') !== 'undefined') {
+        switch ($(this).attr('data-method')) {
+            case 'create':
+                createSection();
+                break;
+            case 'update':
+                updateSection();
+                break;
+            default:
+                break;
+        }
+    }
+
+});
+
+
+function createSection () { 
+    var this_sec = $('#section_select').val().trim();
+    var this_text = $('#section_text').val().trim();
+    var this_img = document.getElementById('section_image');
+    var page_indentifier = $('#page_indentifier').val().trim();
+
+    var formData = new FormData();
+
+    if(this_text == '' || this_text == 'undefined')
+    {
+        Toast.fire({
+            icon: 'error',
+            title: 'Please enter some texts for the section'
+        });
+    }
+    else if(this_img.files.length <= 0)
+    {
+        Toast.fire({
+            icon: 'error',
+            title: 'Please select an image for the section'
+        });
+    }
+
+    else{
+        var dataTarget = $('#is_wanna_for_me').attr('data-action');
+
+        formData.append('this_sec',this_sec);
+        formData.append('section_text',this_text);
+        formData.append('section_image',this_img.files[0]);
+        formData.append('page_indentifier',page_indentifier);
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            type: "POST",
+            url: dataTarget,
+            data: formData,
+            dataType: "json",
+            contentType: false,
+            processData: false,
+            beforeSend: function () {
+                $('#spinner-loader').fadeIn(100);
+                $('#spinner-loader-text').html('Storing...');
+            },
+            success: function (response) {
+                if ((response.data.templates.contents !== '') && (response.data.templates.contents !== 'undefined') && (response.data.templates.contents.length > 0))
+                {
+                    $('#section_image').val(null);
+                    $('#is_wanna_data').val(JSON.stringify(response.data.templates.contents));
+
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Section has been stored successfully'
+                    })
+                }
+                else {
+                    Toast.fire({
+                        icon: 'error',
+                        title: 'Error!! Reload the page and try again'
+                    })
+                }
+            },
+            error: function (request, status, error) {
+                responses = jQuery.parseJSON(request.responseText);
+
+                if (responses.errors) {
+                    var errorHtml = '<ul>';
+                    $.each(responses.errors, function (key, value) {
+                        errorHtml += '<li>' + value + '</li>';
+                    });
+                    errorHtml += '</ul>';
+
+                    Toast.fire({
+                        icon: 'error',
+                        title: errorHtml
+                    })
+
+                }
+            },
+            complete: function () {
+                $('#spinner-loader').fadeOut(100);
+            }
+        });
+    }
+ }
+
+ function updateSection() { 
+
+    var this_sec = $('#section_select').val().trim();
+    var this_text = $('#section_text').val().trim();
+    var this_img = document.getElementById('section_image');
+    var page_indentifier = $('#page_indentifier').val().trim();
+
+    var formData = new FormData();
+
+    if(this_text == '' || this_text == 'undefined')
+    {
+        Toast.fire({
+            icon: 'error',
+            title: 'Please enter some texts for the section'
+        });
+    }
+    else
+    {
+        var dataTarget = $('#is_wanna_for_me').attr('data-action');
+
+        formData.append('this_sec',this_sec);
+        formData.append('section_text',this_text);
+        if(this_img.files.length > 0)
+        {
+            formData.append('section_image',this_img.files[0]);
+        }
+        formData.append('page_indentifier',page_indentifier);
+        formData.append('_method','PUT');
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            type: "POST",
+            url: dataTarget,
+            data: formData,
+            dataType: "json",
+            contentType: false,
+            processData: false,
+            beforeSend: function () {
+                $('#spinner-loader').fadeIn(100);
+                $('#spinner-loader-text').html('Updating...');
+            },
+            success: function (response) {
+                if ((response.data.templates.contents !== '') && (response.data.templates.contents !== 'undefined') && (response.data.templates.contents.length > 0))
+                {
+                    $('#is_wanna_data').val(JSON.stringify(response.data.templates.contents));
+                    $('#section_image').val(null);
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Section has been updated successfully'
+                    })
+                }
+                else {
+                    Toast.fire({
+                        icon: 'error',
+                        title: 'Error!! Reload the page and try again'
+                    })
+                }
+            },
+            error: function (request, status, error) {
+                responses = jQuery.parseJSON(request.responseText);
+
+                if (responses.errors) {
+                    var errorHtml = '<ul>';
+                    $.each(responses.errors, function (key, value) {
+                        errorHtml += '<li>' + value + '</li>';
+                    });
+                    errorHtml += '</ul>';
+
+                    Toast.fire({
+                        icon: 'error',
+                        title: errorHtml
+                    })
+
+                }
+            },
+            complete: function () {
+                $('#spinner-loader').fadeOut(100);
+            }
+        });
+    }
+
+
+  }
+
+function previewImages(src, dest, showSec = '') {
+
+    var total_file = document.getElementById(src).files.length;
+
+    var appendHtml = "<div class='row'>";
+
+    for (let index = 0; index < total_file; index++) {
+
+        appendHtml += "<div class='col-md-4'><img class='rounded previewImg' style='width: 100%; height:200px' src='" + URL.createObjectURL(event.target.files[index]) + "'></div>";
+
+    }
+    $('#' + dest).html(appendHtml + "</div>");
+    if (total_file > 0) {
+
+        $('#' + showSec).fadeIn(600);
+    }
+    else {
+        $('#' + showSec).fadeOut(600);
+    }
+}
---
+++ resources/views/administrator/pages/is_wanna_for_me/index.blade.php
@@ -0,0 +1,119 @@
+<x-administrator-app-layout>
+    <x-slot name="addOnCss">
+
+    </x-slot>
+    <x-slot name='header'>
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Is Wanna For Me</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{ route('administrator.dashboard') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item active">Is Wanna For Me</li>
+                </ol>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                {{-- <button type="button" class="btn btn-block btn-primary btn-lg mt-4" id="hotel_type_create"
+                    style="width: 100px">Create</button> --}}
+
+                <div id="is_wanna_for_me_form_div" style="display:block">
+                    <form action="javscrip:void(0)" id="is_wanna_for_me"
+                        data-slug="{{ $data['templates']->slug ?? null }}" data-action="" method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <input type="hidden" name="page_indentifier" id="page_indentifier" value="is_wanna_for_me">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="name">Select a section</label>
+
+                                <select name="section_select" id="section_select" class="form-control">
+                                    <option value="" selected>Select a section</option>
+                                    <option value="sec_one" selected>Section One</option>
+                                    <option value="sec_two" selected>Section Two</option>
+                                    <option value="sec_three" selected>Section Three</option>
+                                    <option value="sec_four" selected>Section Four</option>
+                                    <option value="sec_five" selected>Section Five</option>
+
+                                    {{-- <optgroup label="Create ?">
+                                        <option value="create">Create a new one +</option>
+                                    </optgroup> --}}
+
+                                    {{-- @if (!empty($data['templates']->contents))
+                                        <optgroup label="Select question">
+                                            @foreach ($data['templates']->contents as $content)
+                                            @endforeach
+                                        </optgroup>
+                                    @endif --}}
+                                </select>
+                                @error('name')
+                                    <span style="color: red">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <div class="section_detailed_div" style="display: none">
+
+                                <div class="form-group">
+                                    <label for="section_text">Section Text</label>
+
+                                    <textarea name="section_text" required id="section_text" cols="70" rows="5" placeholder="Enter section text"
+                                        class="form-control"></textarea>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="slug">Section Image</label>
+
+                                    <input type="file" class="form-control" name="section_image"
+                                        onchange="previewImages('section_image','section_image_preview','section_image_preview_sec')"
+                                        id="section_image">
+                                    @error('section_image')
+                                        <span style="color: red">{{ $message }}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group" id="section_image_preview_sec" style="display: none">
+                                    <label for="section_1_preview_2">Preview</label>
+
+                                    <div id="section_image_preview">
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- /.card-body -->
+
+                        <div style="padding: .75rem 1.25rem; display: flex">
+                            <button type="button" id="sec_button" data-method="create" class="btn btn-primary" disabled>No action
+                                permitted</button>
+
+                            {{-- <div style="display: none" class="fac_active_button">
+                                <div class="bootstrap-switch bootstrap-switch-wrapper bootstrap-switch-animate bootstrap-switch-on bootstrap-switch-focused"
+                                    style="width: 86px;margin-left: 10px;margin-top: 5px;">
+                                    <div class="bootstrap-switch-container" style="width: 126px; margin-left: 0px;">
+                                        <span class="bootstrap-switch-handle-on bootstrap-switch-primary"
+                                            style="width: 42px;">Active</span><span class="bootstrap-switch-label"
+                                            style="width: 42px;">&nbsp;</span><span
+                                            class="bootstrap-switch-handle-off bootstrap-switch-default"
+                                            style="width: 42px;">Deactive</span><input type="checkbox"
+                                            name="faq-active-checkbox" id="faq-active-checkbox" checked="" data-bootstrap-switch=""></div>
+                                </div>
+                            </div> --}}
+                        </div>
+
+                    </form>
+                </div>
+            </div><!-- /.col -->
+
+        </div><!-- /.row -->
+
+        <input type="hidden" name="is_wanna_data" id="is_wanna_data"
+        value="{{ !empty($data['templates']->contents) ? json_encode($data['templates']->contents) : null }}">
+
+    </x-slot>
+
+
+    <x-slot name="addOnJs">
+        <script src="{{ asset('admin/dist/js/pages/is_wanna_for_me.js') }}"></script>
+    </x-slot>
+</x-administrator-app-layout>
